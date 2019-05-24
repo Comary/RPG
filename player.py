f6f8dddf4e7e112f92b45226190fdef8d184fd67
Comary <50996911+Comary@users.noreply.github.com>
2019-05-24 12:18:58 -0400

    player.py

---
+++ player.py
@@ -0,0 +1,20 @@
+# Comary
+# 2019-05-24
+# This is a player object class that allows the user to heal, fight and view inventory
+
+# Imports
+import random
+
+# The Object class
+class player(object):
+    
+    # initialize the attributes
+    def __init__(self):
+        self.hp = 100
+        self.mp = 100
+        self.stam = 100
+        self.dam = 0
+    
+    def heal(self, object):
+        pass
+       
